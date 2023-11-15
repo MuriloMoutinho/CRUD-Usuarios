9c98a4f10f3d266fcec02555362e654c59a7e106
MuriloMoutinho <104277785+MuriloMoutinho@users.noreply.github.com>
2023-11-15 11:35:48 -0300

    angular folder structure

--- src/api/src/main/java/com/apollus/login/security/config/SecurityConfig.java
+++ src/api/src/main/java/com/apollus/login/security/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.apollus.login.security.config;
 
 import com.apollus.login.security.domain.UserRole;
-import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
